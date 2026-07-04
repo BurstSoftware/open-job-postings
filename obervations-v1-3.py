d205336f4e9d08217e932769de5cabef3e569baa
Nathan Rossow <nathanrossow@gmail.com>
2026-07-03 19:06:01 -0500

    Create obervations-v1-3.py

---
+++ obervations-v1-3.py
@@ -0,0 +1,4 @@
+# don't let the future circumstances be defined by the bad requirements of any project.  Build around the requirements within in the same desired result, with different tooling.  Reducing time for everyone that uses this program.
+# don't let the present circumstances be defined by the present requirements for the current step that you are on in the development process.
+
+# requirements 
