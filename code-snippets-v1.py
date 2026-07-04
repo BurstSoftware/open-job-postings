24ee1ac0e6f06468f9adb7747f5e18dd21010f6b
Nathan Rossow <nathanrossow@gmail.com>
2026-07-03 20:50:17 -0500

    Add categories dictionary for code snippets

---
+++ code-snippets-v1.py
@@ -0,0 +1,12 @@
+CATEGORIES = {
+    "All Categories": "All",
+    "Tech": "💻 Tech",
+    "Business": "📊 Business",
+    "Trade": "🔧 Trade & Skilled",
+    "Creative": "🎨 Creative",
+    "Healthcare": "🩺 Healthcare",
+    "Sales": "💼 Sales",
+    "Education": "📚 Education",
+    "Other": "🔀 Other"
+}
+
